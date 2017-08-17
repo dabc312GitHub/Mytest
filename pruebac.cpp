75eac556f6af57338919c17e60edc8ed6a787665
dabc312GitHub <daryl.butron@ucsp.edu.pe>
2017-08-17 14:04:03 -0500

    third commit

--- pruebac.cpp
+++ pruebac.cpp
@@ -1,11 +1,12 @@
-#include <iostream.h>
-using namespace 
+#include <iostream>
+using namespace std;
 int main()
 {
-	int a, b, c, suma;
-		a=5;
-		b=6;
-		c=25;
-		suma = a+ b + c;
-		cout << suma << endl;
-		return 0;}
+    int a, b, c, suma;
+    a=5;
+	b=6;
+	c=25;
+	suma = a+ b + c;
+	cout << suma << endl;
+    return 0;
+}
