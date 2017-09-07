efb51b50b0febbbee157076526a9de0958ff7b37
dabc312GitHub <daryl.butron@ucsp.edu.pe>
2017-09-07 10:56:26 -0500

    Raiz2Multi

---
+++ RaizCuadradaExacta/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+//RAIZ CUADRADA EXACTA y MULTIPLO de 2
+
+int main()
+{
+    int n,a;
+    cin>>n;
+    a=sqrt(n);
+    if(n%2==0 && n/a==a)
+        cout << "si es"<<n << endl;
+    else
+        cout<<"no es exacta y multiplo de 2"<<endl;
+
+    return 0;
+}
+
+    int n
+
+}
+
