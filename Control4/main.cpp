509005caddba9c96f641b2e4986f33b454a1a27a
dabc312GitHub <daryl.butron@ucsp.edu.pe>
2017-09-18 20:56:25 -0500

    terminado

--- Control4/main.cpp
+++ Control4/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    char a[4]={'a','b','c'};
-
-    cout << "Hello world!" << endl;
+    char* a[4]= {"hola,", "Voy a terminar mis tareas","Cerrare el Facebook y WhatsApp!","Bye"};
+    for (int i=4;i>=0;--i)
+        cout<<a[i]<<endl;
     return 0;
 }
